c38071cbe099feb2723c586e0ff4a17195a6ee90
bogdan9861 <besedin_b@bk.ru>
2023-02-05 12:25:30 +0300

    add scroll

--- index.js
+++ index.js
@@ -48,6 +48,8 @@ io.on('connection', (socket) => {
         if (user) {
             io.to(user.room).emit('message', { data: { user, message } })
         }
+
+        window.scrollTo(0, document.body.scrollHeight);
     })
 
     socket.on('leftRoom', ({ params }) => {
